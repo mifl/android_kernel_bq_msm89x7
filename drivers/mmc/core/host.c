23dfab05cffb757d232eb14a7efcc9f842850b75
Mohan Srinivasan <srmohan@google.com>
2016-08-25 17:36:46 -0700

    Android: MMC/UFS IO Latency Histograms.
    This patch adds a new sysfs node (latency_hist) and reports
    IO (svc time) latency histograms. Disabled by default, can be
    enabled by echoing 0 into latency_hist, stats can be cleared
    by writing 2 into latency_hist. This commit fixes the 32 bit
    build breakage in the previous commit. Tested on both 32 bit
    and 64 bit arm devices.
    
    Bug: 30677035
    Change-Id: I06ecca8fe0e784a35738ee19e5ee7d860659e7a9
    Signed-off-by: Mohan Srinivasan <srmohan@google.com>

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -33,7 +33,6 @@
 #include "core.h"
 #include "host.h"
 
-#define cls_dev_to_mmc_host(d)	container_of(d, struct mmc_host, class_dev)
 #define MMC_DEVFRQ_DEFAULT_UP_THRESHOLD 35
 #define MMC_DEVFRQ_DEFAULT_DOWN_THRESHOLD 5
 #define MMC_DEVFRQ_DEFAULT_POLLING_MSEC 100
@@ -801,6 +800,8 @@ int mmc_add_host(struct mmc_host *host)
 		pr_err("%s: failed to create sysfs group with err %d\n",
 							 __func__, err);
 
+	mmc_latency_hist_sysfs_init(host);
+
 	mmc_start_host(host);
 	if (!(host->pm_flags & MMC_PM_IGNORE_PM_NOTIFY))
 		register_pm_notifier(&host->pm_notify);
@@ -830,6 +831,8 @@ void mmc_remove_host(struct mmc_host *host)
 #endif
 	sysfs_remove_group(&host->parent->kobj, &dev_attr_grp);
 	sysfs_remove_group(&host->class_dev.kobj, &clk_scaling_attr_grp);
+
+	mmc_latency_hist_sysfs_exit(host);
 
 	device_del(&host->class_dev);
 
